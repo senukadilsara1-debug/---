36aed1f6678c1d5227be2f92c7802d29717f7865
senukadilsara1-debug <senukadilsara1@gmail.com>
2026-04-09 15:41:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "aV5TWTRB#psXB_CM8bqfEPi4CS8m0-6b52TOBpTBxnmVEoHX1H2s",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
